5d1f22bad938f0a2240959f5a1546dcae928e70b
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-09-29 22:15:17 +0300

    changed geometry, added empty row, changed .pack() into .grid(), changed Button into Radiobutton, IntVar()

--- pagrindinis.py
+++ pagrindinis.py
@@ -6,31 +6,34 @@ from tkinter import *
 
 langas = Tk()
 langas.title("Jausmomatis")
-langas.geometry("500x600")
+langas.geometry("400x500")
 
-busenos = IntVar
+
+busenos_mygtukai = IntVar()
 
 class Pagrindinis:
     def __init__(self, master):
         self.master = master
-        self.pavadinimas = Label(self.master, text="CodeAcademy PTU-5 Jausmomatis")
-        self.pavadinimas.pack()
+        self.pavadinimas = Label(self.master, text="CodeAcademy PTU-5 Jausmomatis", )
+        self.pavadinimas.grid(row=0, columnspan=5)
         self.paaiskinimas = Label(self.master, text="Pasirinkite kaip šiandien jaučiatės išklausius naują temą bei atlikus užduotis")
-        self.paaiskinimas.pack()
-        self.b_liudnas = Radiobutton(self.master, text="Liūdnas", command=self.pasirinktas())
-        self.b_liudnas.pack()
+        self.paaiskinimas.grid(row=1, column=1)
+        self.b_liudnas = Radiobutton(self.master, text="Liūdnas", variable=busenos_mygtukai, value=1, command=self.pasirinktas)
+        self.b_liudnas.grid(row=2, column=1)
         self.liudnas_apibudinimas = Label(self.master, text="visiškai nieko nesuprantu apie ką kalbama,\n perskaičius užduotį neaišku ko manęs prašoma atlikti")
-        self.liudnas_apibudinimas.pack()      
-        self.b_neutralus = Radiobutton(self.master, text="Neutralus", command=self.pasirinktas())
-        self.b_neutralus.pack()
+        self.liudnas_apibudinimas.grid(row=3, column=1)    
+        self.b_neutralus = Radiobutton(self.master, text="Neutralus", variable=busenos_mygtukai, value=2, command=self.pasirinktas)
+        self.b_neutralus.grid(row=4, column=1)
         self.neutralus_apibudinimas = Label(self.master, text="temą kaip ir suprantu, tačiau nepavyksta pilnai/greitai atlikti užduočių")
-        self.neutralus_apibudinimas.pack()
-        self.b_linksmas = Radiobutton(self.master, text="Linksmas", command=self.pasirinktas())
-        self.b_linksmas.pack()
+        self.neutralus_apibudinimas.grid(row=5, column=1)
+        self.b_linksmas = Radiobutton(self.master, text="Linksmas", variable=busenos_mygtukai, value=3, command=self.pasirinktas)
+        self.b_linksmas.grid(row=6, column=1)
         self.linksmas_apibudinimas = Label(self.master, text="viskas aišku ir suprantama!")
-        self.linksmas_apibudinimas.pack()
-        self.b_rezultatas = Button(self.master, text="Rezultatas", command=self.gauti_rezultata())
-        self.b_rezultatas.pack()
+        self.linksmas_apibudinimas.grid(row=7, column=1)
+        self.tarpas = Label(self.master, text='')
+        self.tarpas.grid(row=8, column=1)
+        self.b_rezultatas = Button(self.master, text="Rezultatas", command=self.gauti_rezultata)
+        self.b_rezultatas.grid(row=9, column=1)
 
     def pasirinktas(self):
         pass
