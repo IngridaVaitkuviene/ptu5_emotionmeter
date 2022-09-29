7a3647eea03eab1fe7962ad6013e7cac06964e27
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-09-29 14:14:25 +0300

    main

--- pagrindinis.py
+++ pagrindinis.py
@@ -11,6 +11,15 @@ class Pagrindinis:
         self.pavadinimas.pack()
         self.paaiskinimas = Label(self.master, text="Pasirinkite kaip šiandien jaučiatės išklausius naują temą bei atlikus užduotis")
         self.paaiskinimas.pack()
+        self.b_liudnas = Button(self.master, text="Liūdnas")
+        self.b_neutralus = Button(self.master, text="Neutralus")
+        self.b_linksmas = Button(self.master, text="Linksmas")
+        self.b_liudnas.pack()
+        self.b_neutralus.pack()
+        self.b_linksmas.pack()
+        self.b_rezultatas = Button(self.master, text="Rezultatas")
+        self.b_rezultatas.pack()
+
 
 programa = Pagrindinis(langas)
 langas.mainloop()
